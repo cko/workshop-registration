f7dfabae9d957983dcbfdcc0776971b3b8f47670
Christine Koppelt <cko@users.noreply.github.com>
2016-01-07 08:49:45 +0100

    replace PasswordInfoRepository with db version

--- app/dal/PasswordInfoRepository.scala
+++ app/dal/PasswordInfoRepository.scala
@@ -1,45 +1,65 @@
 package dal
 
+import scala.annotation.implicitNotFound
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits
+import scala.concurrent.Future
+
 import com.mohiva.play.silhouette.api.LoginInfo
 import com.mohiva.play.silhouette.api.util.PasswordInfo
 import com.mohiva.play.silhouette.impl.daos.DelegableAuthInfoDAO
-import dal.PasswordInfoRepository._
-import play.api.libs.concurrent.Execution.Implicits._
 
-import scala.collection.mutable
-import scala.concurrent.Future
+import javax.inject.Inject
+import javax.inject.Singleton
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfig
+import slick.driver.H2Driver.api._
+import slick.driver.JdbcProfile
 
-class PasswordInfoRepository extends DelegableAuthInfoDAO[PasswordInfo] {
+@Singleton
+class PasswordInfoRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) extends DelegableAuthInfoDAO[PasswordInfo] with DBTableDefinitions with HasDatabaseConfig[JdbcProfile] {
+  
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
 
-
-  def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = {
-    Future.successful(data.get(loginInfo))
-  }
-
-  def add(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
-    data += (loginInfo -> authInfo)
-    Future.successful(authInfo)
-  }
-
-  def update(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
-    data += (loginInfo -> authInfo)
-    Future.successful(authInfo)
-  }
-
-  def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
-    find(loginInfo).flatMap {
-      case Some(_) => update(loginInfo, authInfo)
-      case None => add(loginInfo, authInfo)
+  override def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = {
+    val pwQuery = for {
+      dbLoginInfo <- loginInfoQuery(loginInfo)
+      dbPasswordInfo <- slickPasswordInfos.filter(_.loginInfoId === dbLoginInfo.id)
+    } yield dbPasswordInfo
+    db.run(pwQuery.result.headOption).map { dbPasswordInfoOption =>
+      dbPasswordInfoOption.map { pwInfo =>
+        PasswordInfo(pwInfo.hasher, pwInfo.password, pwInfo.salt)
+      }
     }
   }
 
-  def remove(loginInfo: LoginInfo): Future[Unit] = {
-    data -= loginInfo
-    Future.successful(())
-  }
-}
+  def getLoginInfoId(loginInfo: LoginInfo): Future[Long] = db.run {
+    slickLoginInfos.filter(info => info.providerID === loginInfo.providerID && info.providerKey === loginInfo.providerKey).map(_.id)
+      .result.headOption
+  }.map(_.getOrElse(throw new Exception("Associated LoginInfo not found")))
 
-object PasswordInfoRepository {
+  override def update(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] =
+    getLoginInfoId(loginInfo).flatMap {
+      loginInfoId => db.run(slickPasswordInfos.update(DBPasswordInfo(passwordInfo.hasher, passwordInfo.password, passwordInfo.salt, loginInfoId)))
+    }.map(_ => passwordInfo)
 
-  var data: mutable.HashMap[LoginInfo, PasswordInfo] = mutable.HashMap()
+  override def remove(loginInfo: LoginInfo): Future[Unit] =
+    getLoginInfoId(loginInfo).flatMap {
+      loginInfoId => db.run(slickPasswordInfos.filter(_.loginInfoId === loginInfoId).delete)
+    }.map(_ => ())
+
+  override def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] =
+    find(loginInfo).flatMap {
+      case Some(_) => update(loginInfo, authInfo)
+      case None    => add(loginInfo, authInfo)
+    }
+
+  override def add(loginInfo: LoginInfo, passwordInfo: PasswordInfo): Future[PasswordInfo] =
+    getLoginInfoId(loginInfo).flatMap {
+      loginInfoId => db.run(slickPasswordInfos += DBPasswordInfo(passwordInfo.hasher, passwordInfo.password, passwordInfo.salt, loginInfoId))
+    }.map(_ => passwordInfo)
+
+  def passwordInfo2db(loginInfoId: Long, pwInfo: PasswordInfo) = DBPasswordInfo(pwInfo.hasher, pwInfo.password, pwInfo.salt, loginInfoId)
+  def db2PasswordInfo(pwInfo: DBPasswordInfo): PasswordInfo = new PasswordInfo(pwInfo.hasher, pwInfo.password, pwInfo.salt)
+
 }
--- test/dal/PasswordInfoRepositorySpec.scala
+++ test/dal/PasswordInfoRepositorySpec.scala
@@ -6,28 +6,48 @@ import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
 import play.api.Play
+import models.User
+import java.util.UUID
 
 
 class PasswordInfoRepositorySpec extends PlaySpec with ScalaFutures with OneAppPerSuite {
 
   "PasswordInfoRepository" must {
     "store passwordInfo" in {
+      
+      whenReady(storeUser) { user =>
+      
       var repo = Play.current.injector.instanceOf(classOf[PasswordInfoRepository])
       var passwordInfo =  PasswordInfo("", "plainPassword")
-      val loginInfo = LoginInfo(CredentialsProvider.ID, "test@example.com")
-      var futureCreate = repo.save(loginInfo, passwordInfo)
+      var futureCreate = repo.save(user.loginInfo, passwordInfo)
 
       whenReady(futureCreate) { resultPasswordInfo =>
         resultPasswordInfo.password mustBe "plainPassword"
         resultPasswordInfo.hasher mustBe ""
-        var futurePasswordInfo = repo.find(loginInfo)
+        var futurePasswordInfo = repo.find(user.loginInfo)
 
         whenReady(futurePasswordInfo){ resultPasswordInfo =>
           resultPasswordInfo.get.password mustBe "plainPassword"
           resultPasswordInfo.get.hasher mustBe ""
         }
       }
+      }
     }
   }
-
+  
+  def storeUser() = {
+    val email = "test@example.com"
+    val firstname = "firstName"
+    var repo = Play.current.injector.instanceOf(classOf[UserRepository])
+    val loginInfo = LoginInfo(CredentialsProvider.ID, "test@example.com")
+    val uuid = UUID.randomUUID()
+    val user = User(
+            userID = uuid,
+            loginInfo = loginInfo,
+            firstName = Some(firstname),
+            lastName = Some("lastName"),
+            email = Some(email)
+    )
+    repo.save(user)
+  }
 }
