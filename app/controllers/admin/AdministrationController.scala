834471f533ea26a19a38260b66184b52cf0218d8
Christine <cko@users.noreply.github.com>
2016-01-10 00:04:10 +0100

    secure admin area

--- app/controllers/admin/AdministrationController.scala
+++ app/controllers/admin/AdministrationController.scala
@@ -1,17 +1,19 @@
 package controllers.admin
 
 import javax.inject.Inject
-
 import dal.RegistrationRepository
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
-
 import scala.concurrent.ExecutionContext
+import com.mohiva.play.silhouette.api.Silhouette
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import models.User
+import com.mohiva.play.silhouette.api.Environment
 
-class AdministrationController @Inject() (repo: RegistrationRepository, val messagesApi: MessagesApi)
-                                          (implicit ec: ExecutionContext) extends Controller with I18nSupport {
+class AdministrationController @Inject() (repo: RegistrationRepository, val messagesApi: MessagesApi, val env: Environment[User, CookieAuthenticator])
+                                          (implicit ec: ExecutionContext) extends Silhouette[User, CookieAuthenticator] with I18nSupport {
 
-  def getRegistrations = Action.async {
+  def getRegistrations = SecuredAction.async {
     repo.list().map { registrations =>
       Ok(views.html.registered(registrations))
     }
