75600d61a46acbb69e8748d1569353a4500d5d0a
Christine <cko@users.noreply.github.com>
2016-03-26 16:50:39 +0100

    pdf stuff

--- app/controllers/admin/AdministrationController.scala
+++ app/controllers/admin/AdministrationController.scala
@@ -9,8 +9,16 @@ import com.mohiva.play.silhouette.api.Silhouette
 import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
 import models.User
 import com.mohiva.play.silhouette.api.Environment
+import scala.inline
+import service.PdfGenerator
+import scala.inline
+import scala.inline
+import scala.inline
+import java.io.File
+import scala.inline
+import java.io.FileOutputStream
 
-class AdministrationController @Inject() (repo: RegistrationRepository, val messagesApi: MessagesApi, val env: Environment[User, CookieAuthenticator])
+class AdministrationController @Inject() (repo: RegistrationRepository, val pdfGenerator: PdfGenerator, val messagesApi: MessagesApi, val env: Environment[User, CookieAuthenticator])
                                           (implicit ec: ExecutionContext) extends Silhouette[User, CookieAuthenticator] with I18nSupport {
 
   def getRegistrations = SecuredAction.async {
@@ -18,5 +26,18 @@ class AdministrationController @Inject() (repo: RegistrationRepository, val mess
       Ok(views.html.registered(registrations))
     }
   }
+  /*
+  def getConfirmationPdf = SecuredAction.async{
+    val participants = List("")
+    //FIXME
+    val workshop = "Dummy-workshop"
+    val pdfOutputStream = pdfGenerator.generateDownloadPdf(participants, workshop)
+    val pdfContent = new File("")  
+    
+     var fos = new FileOutputStream (pdfContent); 
+     pdfOutputStream.writeTo(fos);
+  
+    Ok.sendFile(pdfContent, false, fileName = _ => "teilnahmebestaetigungen.pdf")
+  }*/
 }
 
